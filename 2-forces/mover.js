e40ee3eebf50fcdc17694e18169bea437bda828d
emsa16 <emil.hietanen@gmail.com>
2025-02-16 22:45:46 +0100

    refactor Mover class to include friction and improve edge collision handling

--- 2-forces/mover.js
+++ 2-forces/mover.js
@@ -1,13 +1,15 @@
-const MASS_CONSTANT = 16;
+const MASS_COEFFICIENT = 16;
+const NORMAL_FORCE = 1;
 
 class Mover {
-  constructor(mass, x, y) {
+  constructor(mass, x, y, frictionCoefficient) {
     this.mass = mass;
-    this.diameter = mass * MASS_CONSTANT;
+    this.diameter = mass * MASS_COEFFICIENT;
     this.radius = this.diameter / 2;
     this.position = createVector(x, y);
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
+    this.frictionMag = frictionCoefficient * NORMAL_FORCE;
   }
 
   applyForce(force) {
@@ -28,20 +30,34 @@ class Mover {
   }
 
   checkEdges() {
+    const bounce = 0.95;
+
     if (this.position.x + this.radius > width) {
       this.position.x = width - this.radius;
-      this.velocity.x *= -1;
+      this.velocity.x *= -1 * bounce;
     } else if (this.position.x - this.radius < 0) {
       this.position.x = 0 + this.radius;
-      this.velocity.x *= -1;
+      this.velocity.x *= -1 * bounce;
     }
 
     if (this.position.y + this.radius > height) {
       this.position.y = height - this.radius;
-      this.velocity.y *= -1;
+      this.velocity.y *= -1 * bounce;
     } else if (this.position.y - this.radius < 0) {
       this.position.y = 0 + this.radius;
       this.velocity.y *= -1;
+    }
+  }
+
+  contactEdge() {
+    return this.position.y > height - this.radius - 1;
+  }
+
+  applyFriction() {
+    if (this.contactEdge()) {
+      const friction = p5.Vector.mult(this.velocity, -1);
+      friction.setMag(this.frictionMag);
+      this.applyForce(friction);
     }
   }
 }
--- 2-forces/sketch.js
+++ 2-forces/sketch.js
@@ -1,19 +1,21 @@
 // let balloon;
 let moverA;
 let moverB;
+let gravity;
 
 function setup() {
   createCanvas(640, 640);
   // balloon = new Balloon();
-  moverA = new Mover(1, width / 2, 30);
-  moverB = new Mover(3, 500, 100);
+  moverA = new Mover(1, width / 2, 30, 0.1);
+  moverB = new Mover(3, 500, 100, 2);
+
+  gravity = createVector(0, 0.1);
 }
 
 function draw() {
   background(200);
   // balloon.update();
   // balloon.show();
-  const gravity = createVector(0, 0.1);
 
   if (mouseIsPressed) {
     // v1
@@ -22,49 +24,58 @@ function draw() {
     // moverB.applyForce(wind);
 
     // v2
-    const mouse = createVector(mouseX, mouseY);
+    const wind = createVector(mouseX, mouseY);
     const center = createVector(width / 2, height / 2);
-    mouse.sub(center);
-    mouse.mult(-1);
-    mouse.setMag(0.1);
-    moverA.applyForce(mouse);
-    moverB.applyForce(mouse);
+    wind.sub(center);
+    wind.mult(-1);
+    wind.setMag(0.1);
+    moverA.applyForce(wind);
+    moverB.applyForce(wind);
+
+    // // v3 tossing the mover
+    // const toss = createVector(mouseX, mouseY);
+    // toss.sub(moverA.position);
+    // toss.mult(-1);
+    // toss.setMag(4);
+    // moverA.velocity = toss;
   }
 
   const gravityA = p5.Vector.mult(gravity, moverA.mass);
   moverA.applyForce(gravityA);
-  const wallForceA = calculateWallForce(moverA);
-  moverA.applyForce(wallForceA);
+  // const wallForceA = calculateWallForce(moverA);
+  // moverA.applyForce(wallForceA);
+  moverA.applyFriction();
   moverA.update();
   moverA.show();
-  // moverA.checkEdges();
+  moverA.checkEdges();
 
   const gravityB = p5.Vector.mult(gravity, moverB.mass);
-  const wallForceB = calculateWallForce(moverB);
-  moverB.applyForce(wallForceB);
   moverB.applyForce(gravityB);
+  // const wallForceB = calculateWallForce(moverB);
+  // moverB.applyForce(wallForceB);
+  moverB.applyFriction();
   moverB.update();
   moverB.show();
-  // moverB.checkEdges();
+  moverB.checkEdges();
 }
 
-function calculateWallForce(mover) {
-  const distanceToRightWall = width - mover.position.x + mover.radius;
-  const distanceToLeftWall = mover.position.x - mover.radius;
-  const distanceToTopWall = mover.position.y - mover.radius;
-  const distanceToDownWall = height - mover.position.y + mover.radius;
+// function calculateWallForce(mover) {
+//   const distanceToRightWall = width - mover.position.x + mover.radius;
+//   const distanceToLeftWall = mover.position.x - mover.radius;
+//   const distanceToTopWall = mover.position.y - mover.radius;
+//   const distanceToDownWall = height - mover.position.y + mover.radius;
 
-  const rightWallForce = createVector((-1 / distanceToRightWall) * 50, 0);
-  const leftWallForce = createVector((1 / distanceToLeftWall) * 50, 0);
-  const topWallForce = createVector(0, (1 / distanceToTopWall) * 50);
-  const downWallForce = createVector(0, (-1 / distanceToDownWall) * 50);
+//   const rightWallForce = createVector((-1 / distanceToRightWall) * 50, 0);
+//   const leftWallForce = createVector((1 / distanceToLeftWall) * 50, 0);
+//   const topWallForce = createVector(0, (1 / distanceToTopWall) * 50);
+//   const downWallForce = createVector(0, (-1 / distanceToDownWall) * 50);
 
-  const wallForce = createVector(0, 0);
+//   const wallForce = createVector(0, 0);
 
-  wallForce
-    .add(rightWallForce)
-    .add(leftWallForce)
-    .add(topWallForce)
-    .add(downWallForce);
-  return wallForce;
-}
+//   wallForce
+//     .add(rightWallForce)
+//     .add(leftWallForce)
+//     .add(topWallForce)
+//     .add(downWallForce);
+//   return wallForce;
+// }
